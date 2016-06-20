f5f47c3861023c59532a4fcf6ccd1ab6ba54cef7
Sean Norton <NortonSeanM@gmail.com>
2016-06-20 16:45:09 -0500

    Attack enemy or walk

--- rubywarrior/margot-the-great-beginner/player.rb
+++ rubywarrior/margot-the-great-beginner/player.rb
@@ -1,5 +1,15 @@
 class Player
+
   def play_turn(warrior)
-    warrior.walk!
+    if is_enemy?(warrior)
+      warrior.attack!
+    else
+      warrior.walk!
+    end
   end
+
+  def is_enemy?(warrior)
+    warrior.feel.enemy?
+  end
+
 end
